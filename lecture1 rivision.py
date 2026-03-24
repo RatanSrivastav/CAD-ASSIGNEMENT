003ef17f584fb72ce016db201b264c5fa75bf2b4
Ratan Srivastav <ratansrivastav31@gmail.com>
2026-03-24 20:54:08 +0530

    Add merge sort implementation to lecture1 revision

--- lecture1 rivision.py
+++ lecture1 rivision.py
@@ -1,8 +1,44 @@
-#  write a program to input 2 floating & print their average
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
 
-a=float(input("enter a"))
-b=float(input("enter b"))
-average=(a+b)/2
-print(average)
+    L = arr[left:mid+1]
+    R = arr[mid+1:right+1]
 
+    i = j = 0
+    k = left
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+
+# Example
+arr = [5, 2, 9, 1, 3]
+merge_sort(arr, 0, len(arr)-1)
+print("Sorted:", arr)
 
